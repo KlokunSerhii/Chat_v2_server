c54b23d476a18f22a04975a8b718ceb7fd36df24
KlokunSerhii <klokuns@gmail.com>
2025-06-25 13:39:37 +0300

    messageSchema

--- models/Message.js
+++ models/Message.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const messageSchema = new mongoose.Schema({
   sender: { type: String, required: true },
-  text: { type: String, required: true },
+  text: { type: String, required: false },
   username: { type: String, required: true },
   avatar: { type: String },
   image: { type: String },
