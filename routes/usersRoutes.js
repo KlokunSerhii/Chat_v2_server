27ca9ae288ad36de129ebd623b94878b9b938f5d
Serhii Klokun <klokuns@gmail.com>
2025-07-03 00:10:42 +0300

    add all users

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -6,7 +6,13 @@ import { authenticateToken } from "../middleware/middleware.js";
 const router = express.Router();
 
 router.get("/users", authenticateToken, async (req, res) => {
-  const users = await User.find({}, "id username avatar");
+  try {
+    const users = await User.find({}, "id username avatar");
+    res.json({ users }); // <-- ось що потрібно
+  } catch (error) {
+    console.error("Помилка при отриманні користувачів:", error);
+    res.status(500).json({ error: "Помилка сервера" });
+  }
 });
 
 export default router;
