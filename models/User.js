27468755fe54b76525ca9a297081b133c3b13d05
Serhii Klokun <klokuns@gmail.com>
2025-07-02 23:56:06 +0300

    add all users

--- models/User.js
+++ models/User.js
@@ -1,9 +1,19 @@
 import mongoose from "mongoose";
+import { type } from "os";
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   passwordHash: { type: String, required: true },
   avatar: { type: String, required: true },
+});
+
+// Додаємо віртуальне поле id, яке повертає _id як рядок
+userSchema.virtual("id").get(function () {
+  return this._id.toHexString();
+});
+
+userSchema.set("toJSON", {
+  virtuals: true,
 });
 
 const User = mongoose.model("User", userSchema);
